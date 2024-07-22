0936343b974af4132d767ffb1a955ddbc85cd74c
moff <mswelatumelo@gmail.com>
2024-07-22 15:26:28 +0200

    implemented default remember me to test functionality

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -46,8 +47,9 @@ public class ApplicationSecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/courses", true)
-                );
+                        .defaultSuccessUrl("/courses", true))
+
+                .rememberMe(Customizer.withDefaults());
 
         return http.build();
 
