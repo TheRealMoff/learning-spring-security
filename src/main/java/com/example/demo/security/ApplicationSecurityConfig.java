066d9c819034370e58c722c666ab3d30ca4bbdba
moff <mswelatumelo@gmail.com>
2024-07-22 15:56:26 +0200

    implemented custom logout using logoutRequestMatcher

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -53,7 +54,15 @@ public class ApplicationSecurityConfig {
 
                 .rememberMe(remember -> remember
                         .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                        .key("somethingVerySecured")
+                        .key("somethingVerySecured"))
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // always delete when csrf is enabled to use POST
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .logoutSuccessUrl("/login")
                 );
 
         return http.build();
